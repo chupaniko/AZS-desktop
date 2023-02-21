17733fc1bcbbb6b8d2d9cb6d1a7693651b80765f
Nikolai Chupakhin <nikolaicxxi@gmail.com>
2023-02-21 20:34:37 +0400

    add changes

--- src/main/java/com/example/azs_fx_1/TopologyParamsController.java
+++ src/main/java/com/example/azs_fx_1/TopologyParamsController.java
@@ -1,4 +1,33 @@
 package com.example.azs_fx_1;
 
+import com.example.azs_fx_1.dto.TopologyDTO;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class TopologyParamsController {
+
+    public TextField topologyName;
+    public TextField length_AZS;
+    public TextField width_AZS;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void onSetTopologyParamsClick(ActionEvent actionEvent) throws IOException {
+        TopologyDTO topologyDTO = new TopologyDTO(topologyName.getText(), Integer.parseInt(length_AZS.getText()), Integer.parseInt(width_AZS.getText()));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("topologyParams.fxml"));
+        root = loader.load();
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setTitle("Настройка служебной области АЗС");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
